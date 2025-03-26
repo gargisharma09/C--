1389ba977e8526bd10285ada99a761dcab0dd1f7
gargisharma09 <sharma931g@gmail.com>
2025-03-26 14:49:26 +0530

    Adding getDetails and displayDetails

--- class.cpp
+++ class.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
 using namespace std;
 
+class Student{
+    string name;
+    int rollno;
+    int regno;
+
+public:
+    int getDetails(string n, int r, int reg){
+        name = n;
+        rollno = r;
+        regno = reg;
+    }
+    int getDetailsuser(){
+        cout << "Enter the Name: ";
+        cin >> name;
+        cout << "Enter the Roll No: ";
+        cin >> rollno;
+        cout << "Enter the Registration no: ";
+        cin >> regno;
+    }
+
+    void displayDetails(){
+        cout << "Name: " << endl;
+        cout << "Roll No: " << endl;
+        cout << "Registration No: " << endl;
+    }
+
+};
 
 int main(){
-    
+    Student s1;
+    s1.getDetailsuser();
     return 0;
 }
