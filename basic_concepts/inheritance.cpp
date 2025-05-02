e8ae42e1c4ce41519120ab6bd23b2255ba512c6d
gargisharma09 <sharma931g@gmail.com>
2025-05-02 13:33:45 +0530

    new work

---
+++ basic_concepts/inheritance.cpp
@@ -0,0 +1,32 @@
+class Customer:
+   def set_name(self, name):
+        self.name = name
+
+   
+    def show_customer(self):
+        print("Customer Name:", self.name)
+
+
+class OrderDetails:
+
+    def set_product(self, product):
+        self.product = product
+
+   
+    def show_order(self):
+        print("Product Ordered:", self.product)
+
+
+class OnlinePurchase(Customer, OrderDetails):
+   
+    def set_details(self, name, product):
+        self.set_name(name)
+        self.set_product(product)
+
+   
+    def show_purchase(self):
+        self.show_customer()
+        self.show_order()
+
+
+
