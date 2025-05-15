38ffb958025725b9b04ebf38a43c6c99fa05d55a
gargisharma09 <sharma931g@gmail.com>
2025-05-15 21:35:11 +0530

    new question

--- basic_concepts/pointers.cpp
+++ basic_concepts/pointers.cpp
@@ -13,6 +13,20 @@ int main(){
 using namespace std;
 
 int main(){
-    
+    int *ptr;
+    int x;
+    ptr = &x;
+    *ptr = 0;
+    cout << "Value of x: " << x << endl;
+    cout << "Value of *ptr: " << *ptr << endl;
+
+    *ptr += 5;
+    cout << "Value of x : " << x << endl;
+    cout << "*ptr: " << *ptr << endl;
+
+    (*ptr)++;
+    cout << "Value of x : " << x << endl;
+    cout << "*ptr: " << *ptr << endl;
+
     return 0;
 }
