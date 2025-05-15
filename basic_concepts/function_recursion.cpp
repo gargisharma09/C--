1868b8b48e5d67fc657276778006ad7bc3d87f64
gargisharma09 <sharma931g@gmail.com>
2025-05-15 11:56:15 +0530

    new

---
+++ basic_concepts/function_recursion.cpp
@@ -0,0 +1,16 @@
+/*#include <iostream>
+using namespace std;
+
+//declare function/protyping
+void printhello();
+
+int main(){
+    printhello();//function call
+    return 0;
+}
+// function definition
+void printhello(){
+    cout << "hello world" << endl;
+    printhello();
+}*/
+
