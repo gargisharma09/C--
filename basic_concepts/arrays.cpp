31d47c0930fde89ac75b77ade7c5f485ce5c5fed
gargisharma09 <sharma931g@gmail.com>
2025-05-15 23:20:59 +0530

    array

---
+++ basic_concepts/arrays.cpp
@@ -0,0 +1,32 @@
+/*Arrays: collections of similar data types stored at contiguous memory locations*/
+/*#include <iostream>
+using namespace std;    
+int main(){
+    int arr[5] = {1, 2, 3, 4, 5};
+    cout << "Array elements: ";
+    for(int i = 0; i < 5; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    
+    // Accessing array elements using pointers
+    int *ptr = arr;
+    cout << "Array elements using pointers: ";
+    for(int i = 0; i < 5; i++){
+        cout << *(ptr + i) << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}   */
+
+#include <iostream>
+using namespace std;        
+int main(){
+    int marks[3] = {99,95,94};
+    cout << "Marks: ";
+    for(int i = 0; i < 3; i++){
+        cout << marks[i] << " ";
+    }
+    return 0;
+}
