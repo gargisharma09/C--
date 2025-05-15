57147b0de53425e63453e57f3b6c4281469f2c8d
gargisharma09 <sharma931g@gmail.com>
2025-05-15 18:43:34 +0530

    pointers

---
+++ basic_concepts/pointers.cpp
@@ -0,0 +1,18 @@
+/*#include <iostream>
+using namespace std;
+
+int main(){
+    int age = 22;
+    int *ptr= &age;
+    int _age= *ptr;
+    cout<<"Age: "<<age<<endl;
+    cout<<"Pointer: "<<ptr<<endl;
+    return 0;
+}*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
