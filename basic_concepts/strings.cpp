37da185c28d42ba3856f1bc3c25fca85342ee380
gargisharma09 <sharma931g@gmail.com>
2025-05-18 19:29:17 +0530

    string

---
+++ basic_concepts/strings.cpp
@@ -0,0 +1,21 @@
+/*#include <iostream>
+using namespace std;
+
+int main(){
+    char name[]= {'g','a','r','g','i','\0'};
+    cout << "The name is: " << name << endl;
+    //remeber to use \0 with''    
+    return 0;
+}*/
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+    char name[]= {"gargi"};
+    cout << "The name is: " << name << endl;
+    //remember with "" no need to use \0
+}*/
+
+#include <iostream>
+using namespace std;
