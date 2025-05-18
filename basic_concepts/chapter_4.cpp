df7119cbe55c8cf23adf9e135870e9b6ed0d8001
gargisharma09 <sharma931g@gmail.com>
2025-05-18 21:29:17 +0530

    pattern

--- basic_concepts/chapter_4.cpp
+++ basic_concepts/chapter_4.cpp
@@ -15,7 +15,7 @@ int main(){
     return 0;
 }*/
 
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 
 int main(){
@@ -29,5 +29,20 @@ int main(){
         cout<<endl;
     }
     
+    return 0;
+}*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n=4;
+    for(int i=0; i<n; i++){
+
+        for(int j=0; j<i+1; j++){
+            cout<<"*";
+        }
+
+        cout<<endl;
+    }
     return 0;
 }
