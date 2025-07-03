4a95e657e6557b1c288b5603d4838b49451a0bb7
gargisharma09 <sharma931g@gmail.com>
2025-07-03 22:20:04 +0530

    vector DSA

---
+++ basic_concepts/vector.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+int main(){
+    vector<int> vec= {1,2,3}; // Declare a vector of integers    
+    cout<< vec[2] << endl; // Access the first element of the vector
+    return 0;
+}
