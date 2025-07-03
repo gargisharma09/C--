766ebb40fc1ea152c58a6055038e922d99beed90
gargisharma09 <sharma931g@gmail.com>
2025-07-03 22:24:52 +0530

    vector

--- basic_concepts/vector.cpp
+++ basic_concepts/vector.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 #include <vector>
 
 
@@ -7,5 +7,18 @@ using namespace std;
 int main(){
     vector<int> vec= {1,2,3}; // Declare a vector of integers    
     cout<< vec[2] << endl; // Access the first element of the vector
+    return 0;
+}*/
+#include <iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+    vector<int>vec (4,0);
+    for(int i:vec) {
+        cout<<i<< endl;
+    }
+    
     return 0;
 }
