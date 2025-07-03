59cdfaa86c7c2763eb5bd0da42ed746b90cc7408
gargisharma09 <sharma931g@gmail.com>
2025-07-03 10:11:24 +0530

    arraysDSA

---
+++ basic_concepts/arraysDSA.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int marks[5] = {1, 2, 3, 4, 5}; 
+    
+
+    cout << "Marks of student 1: " << marks[0] << endl; // Accessing the first element of the array
+    cout << "Marks of student 2: " << marks[1] << endl;
+    //Declaration and initialization of an array
+    return 0;
+}
