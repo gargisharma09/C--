5468fd43bd3c9af8416392cfa31d93f49702f244
gargisharma09 <sharma931g@gmail.com>
2025-07-06 11:54:38 +0530

    subarray

--- basic_concepts/vector.cpp
+++ basic_concepts/vector.cpp
@@ -22,7 +22,7 @@ int main(){
     
     return 0;
 }*/
-#include <iostream>
+/*#include <iostream>
 #include<vector>
 using namespace std;
 
@@ -33,4 +33,20 @@ int main(){
     }
     
     return 0;
+}*/
+
+#include<iostream>
+#include<vector>
+using namespace std;        
+
+int main() {
+    vector<int> vec;
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
+    vec.push_back(4);
+    vec.push_back(5);
+
+    cout<< vec.size() << endl; // Output the size of the vector
+    cout<< vec.capacity() << endl; // Output the capacity of the vector
 }
