26c363775f6b09a99fdc5cc713fadeef1a063e55
gargisharma09 <sharma931g@gmail.com>
2025-07-10 20:13:57 +0530

    subarray

--- basic_concepts/subarray.cpp
+++ basic_concepts/subarray.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 
 int main(){
@@ -15,4 +15,56 @@ int main(){
         }
     }
     return 0;
+}*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n = 5;
+    int arr[n] = {1, 2, 3, 4, 5};
+    int maxSum =  INT_MIN;
+
+    for(int st = 0; st < n; st++){
+        int currentSum = 0;
+        for(int end = st; end < n; end++){
+            currentSum += arr[end];
+            cout << "Subarray from index " << st << " to " << end << ": ";
+            for(int i = st; i <= end; i++){
+                cout << arr[i] << " ";
+            }
+            cout << " | Current Sum: " << currentSum << endl;
+            maxSum = max(maxSum, currentSum);
+        }
+    
+    }
+    return 0;
+}*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n = 5;
+    int arr[n] = {1, 2, 3, 4, 5};
+    int maxSum = INT_MIN;
+
+    for(int st = 0; st < n; st++){
+        int currentSum = 0;
+        for(int end = st; end < n; end++){
+            currentSum += arr[end];
+            cout << "Subarray from index " << st << " to " << end << ": ";
+            for(int i = st; i <= end; i++){
+                cout << arr[i] << " ";
+            }
+            cout << " | Current Sum: " << currentSum << endl;
+            maxSum = max(maxSum, currentSum);
+        }   
+    }
+    cout << "Maximum Subarray Sum: " << maxSum << endl; 
+    
+    
+    return 0;
 }
+
+
